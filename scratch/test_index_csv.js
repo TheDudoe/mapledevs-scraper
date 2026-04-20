8bd235c9b0f1f85dc66fb8a8d90b0dc5192e1f05
TheDudoe <mapledevsbusiness@gmail.com>
2026-04-20 00:07:27 -0700

    chore: cleanup scratch files and update .gitignore

--- scratch/test_index_csv.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const indexHtml = fs.readFileSync('index.html', 'utf8');
-const match = indexHtml.match(/function parseCSV\(t\)\{.*?\}/)[0];
-
-console.log("Length:", match.length);
-console.log("Snippet:", match.substring(0, 150));
-console.log("Snippet end:", match.substring(match.length - 150));
